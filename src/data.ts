c9c850c158ac16c4e701c8c792d182881906de8e
11SF <nawaphong.sit@dome.tu.ac.th>
2021-10-15 22:01:12 +0700

    add data

--- src/data.ts
+++ src/data.ts
@@ -4,160 +4,145 @@ export const ogImageUrl =
   "https://og-image.vercel.app/Resumette.png?theme=light&md=1&fontSize=200px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-black.svg"
 
 export const introData = {
-  name: "Manassarn Manoonchai",
-  nickname: "Noom",
-  phone: "+66863949474",
-  email: "manassarn@gmail.com",
-  github: "narze",
-  linkedin: "manassarnmanoonchai",
-  location: "Bangkok, Thailand",
-  website: "monosor.com",
+  name: "Nawapong Sitaruno",
+  nickname: "Folk",
+  phone: "+66623510099",
+  email: "nsfolknsf@gmail.com",
+  github: "11SF",
+  linkedin: "11SF",
+  location: "Nakhon Si Thammarat, Thailand",
+  website: "11sf-web.netlify.app/",
 }
 
 export const technologies = [
   {
     section: "Languages:",
     details:
-      "Ruby, JavaScript, Node.js (strong). TypeScript, SQL (proficient).",
+      "C, C++, Java, Python, JavaScript, Dart",
   },
   {
     section: "Frameworks:",
     details:
-      "Ruby on Rails, ReactJS, NextJS, NestJS, Svelte, TailwindCSS, Bootstrap",
+      "ReactJS, VueJS, TailwindCSS, Bootstrap, ExpressJS, Flutter",
   },
   {
     section: "Tools:",
     details:
-      "Git, Terraform, Docker, Kubernetes, Helm, ArgoCD, FluxCD, Robot Framework, Selenium, Zsh, GitHub Codespaces",
+      "Git, Docker, Selenium, Zsh",
   },
   {
     section: "Other:",
     details:
-      "AWS, GCP, Firebase, Vercel, DigitalOcean, Cloud66, Github Actions, CircleCI, CloudFlare",
+      "AWS, Firebase, Vercel, Netlify, Heroku",
   },
 ]
 
 export const educations = [
   {
-    head: "B.Eng. Computer Engineering",
-    details: "Chulalongkorn University, TH. 2007-2010 (GPA 3.45)",
+    head: "Computer Science",
+    details: "Thammasat University, Rungsit TH. 2007-2010 (GPA 3.43)",
   },
 ]
 
-export const workExperiences = [
-  {
-    position: "Full-stack Developer, Co-founder",
-    company: "EventPop",
-    url: "https://eventpop.me",
-    years: ["2015", "Current"],
-    details: [
-      "Designed, developed and maintained several core services for EventPop website (Ticketing, Seating, Payment, Fee Calculation, Line Item, Billing, Reporting, APIs, etc.) using Ruby on Rails",
-      "Researched & applied improvements for development workflow. Testing libraries : RSpec, Guard, VCR, Capybara, Selenium, Cucumber, Timecop. CI/CD : CircleCI, Github Actions, Drone CI, ArgoCD, FluxCD, Terraform",
-      "Maintained server operations, deployments, containerization with Docker & Kubernetes, CI/CD. Also pioneered DevOps practices in the team",
-      "Integrated 3rd-party services : Kerry Express, DHL, Queue-It, Refund Protect, Skootar, Tanita Body Scale",
-      "Integrated payment service providers : Omise, ThaiEPay, 123 by 2C2P, SCB-Easy, K-PayPlus, K-PaymentGateway",
-    ],
-  },
-  {
-    position: "Maker, Co-founder",
-    company: "Maker Zoo",
-    url: "https://fb.me/makerzoo",
-    years: ["2014", "2016"],
-    details: [
-      "Pioneered Maker Movement in Thailand by opening first of the Makerspaces in Bangkok",
-      "Hosted maker workshops eg. 3D-modeling, 3D-printing, Arduino, IoT, etc",
-      "Developed hardware-related products for clients and startups",
-      "Developed smart door lock system for Hubba, a co-working space in Bangkok",
-      "Provided 3D-printing service & consultant",
-    ],
-  },
-  {
-    position: "Web / IoT Developer (Contract)",
-    company: "Box24",
-    url: "https://washbox24.com",
-    years: ["2015", "2016"],
-    details: [
-      "Developed internet connected Smart Lockers System using web technologies (Node.js, React.js) on embedded systems (Ubuntu Linux, Arduino, Raspberry Pi)",
-      "Developed private Node.js library to connect with Nayax via serial port to receive cashless payment on vending machines, and also supported Thai Baht cash payment",
-    ],
-  },
-  {
-    position: "Web Developer",
-    company: "Figabyte",
-    url: "https://www.facebook.com/figabyte",
-    years: ["2011", "2014"],
-    details: [
-      "Developed several Facebook web apps with CodeIgniter, using Facebook API",
-      "Developed web platform using Ruby on Rails 3 & 4",
-      "Made iOS apps for clients using PhoneGap and RubyMotion",
-    ],
-  },
-  {
-    position: "Part-time Web Developer",
-    company: "Wongnai Media (wongnai.com)",
-    url: "https://wongnai.com",
-    years: ["2010"],
-    details: [
-      "Developed a small part of website using Java with Spring MVC Framework built with Apache Maven",
-      "Learned TDD practice & making early-stage startup",
-    ],
-  },
-]
+// export const workExperiences = [
+//   {
+//     position: "Full-stack Developer, Co-founder",
+//     company: "EventPop",
+//     url: "https://eventpop.me",
+//     years: ["2015", "Current"],
+//     details: [
+//       "Designed, developed and maintained several core services for EventPop website (Ticketing, Seating, Payment, Fee Calculation, Line Item, Billing, Reporting, APIs, etc.) using Ruby on Rails",
+//       "Researched & applied improvements for development workflow. Testing libraries : RSpec, Guard, VCR, Capybara, Selenium, Cucumber, Timecop. CI/CD : CircleCI, Github Actions, Drone CI, ArgoCD, FluxCD, Terraform",
+//       "Maintained server operations, deployments, containerization with Docker & Kubernetes, CI/CD. Also pioneered DevOps practices in the team",
+//       "Integrated 3rd-party services : Kerry Express, DHL, Queue-It, Refund Protect, Skootar, Tanita Body Scale",
+//       "Integrated payment service providers : Omise, ThaiEPay, 123 by 2C2P, SCB-Easy, K-PayPlus, K-PaymentGateway",
+//     ],
+//   },
+//   {
+//     position: "Maker, Co-founder",
+//     company: "Maker Zoo",
+//     url: "https://fb.me/makerzoo",
+//     years: ["2014", "2016"],
+//     details: [
+//       "Pioneered Maker Movement in Thailand by opening first of the Makerspaces in Bangkok",
+//       "Hosted maker workshops eg. 3D-modeling, 3D-printing, Arduino, IoT, etc",
+//       "Developed hardware-related products for clients and startups",
+//       "Developed smart door lock system for Hubba, a co-working space in Bangkok",
+//       "Provided 3D-printing service & consultant",
+//     ],
+//   },
+//   {
+//     position: "Web / IoT Developer (Contract)",
+//     company: "Box24",
+//     url: "https://washbox24.com",
+//     years: ["2015", "2016"],
+//     details: [
+//       "Developed internet connected Smart Lockers System using web technologies (Node.js, React.js) on embedded systems (Ubuntu Linux, Arduino, Raspberry Pi)",
+//       "Developed private Node.js library to connect with Nayax via serial port to receive cashless payment on vending machines, and also supported Thai Baht cash payment",
+//     ],
+//   },
+//   {
+//     position: "Web Developer",
+//     company: "Figabyte",
+//     url: "https://www.facebook.com/figabyte",
+//     years: ["2011", "2014"],
+//     details: [
+//       "Developed several Facebook web apps with CodeIgniter, using Facebook API",
+//       "Developed web platform using Ruby on Rails 3 & 4",
+//       "Made iOS apps for clients using PhoneGap and RubyMotion",
+//     ],
+//   },
+//   {
+//     position: "Part-time Web Developer",
+//     company: "Wongnai Media (wongnai.com)",
+//     url: "https://wongnai.com",
+//     years: ["2010"],
+//     details: [
+//       "Developed a small part of website using Java with Spring MVC Framework built with Apache Maven",
+//       "Learned TDD practice & making early-stage startup",
+//     ],
+//   },
+// ]
 
-export const projects = [
+export const termProjects = [
   {
     name: "Manoonchai",
     details:
       "Modern, productive, and data-driven Thai keyboard layout. A collection of projects built with TypeScript, Elixir, Svelte",
     url: "manoonchai.com",
   },
+]
+
+export const sideProjects = [
   {
-    name: "9armbot (Contributor)",
+    name: "Family-Pay",
     details:
-      "Twitch & Discord bot for 9arm (Thai technology Youtuber). I helped re-structuring the project using TypeScript & Prisma",
-    url: "github.com/thananon/twitch_tools",
+      "Member management system, for display outstanding balance and make a payment record of spotify family and youtube premium service, built with ReactJs, TailwindCSS and Backend:ExpressJS",
+    url: "https://github.com/11SF/Family-Pay",
   },
   {
-    name: "Dotfiles",
+    name: "Spotify-pay-flutter",
     details:
-      "macOS dotfiles with setup script for self use, tested on Apple Silicon Macs (Zsh, Zinit, Dotbot, GNU Make, etc.)",
-    url: "github.com/narze/dotfiles",
+      "Similar Family-Pay Project but it can show only one family, built with Flutter",
+    url: "https://github.com/11SF/Spotify-pay-flutter",
   },
   {
-    name: "#100DaysOfCode",
+    name: "Netflix-Clone",
     details:
-      "My take on practicing modern frontend development by building small projects in 100 days",
-    url: "100daysofcode-narze.vercel.app",
+      "Clone design of Netflix built with ReactJS and TailwindCSS",
+    url: "https://github.com/11SF/Netflix-Clone",
   },
   {
-    name: "Baht.rb",
-    details: "Ruby gem to convert number to Thai Baht format",
-    url: "rubygems.org/gems/baht",
-  },
-  {
-    name: "Resumette",
+    name: "POPGOP",
     details:
-      "Interactive & printable résumé with tailored skills fitted in single letter page (This page!)",
-    url: "manassarn-resume.vercel.app",
+      "Clone of popcat.click meme clicking game built with ReactJS",
+    url: "https://github.com/11SF/POPGOP",
   },
-  {
-    name: "Popyut",
-    details:
-      "Contributed to a clone of popcat.click meme clicking game, gone viral and got 2M visitors within one week",
-    url: "github.com/popyut/popyut",
-  },
-  {
-    name: "Resound",
-    details: "Remote soundboard for meetings or streaming. Uses WebRTC instead of server to host rooms.",
-    url: "github.com/narze/resound",
-  }
 ]
 
 export const interests = [
-  "Areas : Coding, Productivity & Life-hacks, Mechanical Keyboards",
-  "Typing : 120+ words per minute (Colemak Mod-DH layout)",
-  "Games : Music games, FPS, MOBA, Puzzle",
-  "Music : EDM, Rock, Japanese",
-  "Remote working is a OK. I extensively use self time tracking app for all my works.",
-  "Pets : I have 3 dogs",
+  "Freetime : Brew Coffee, Guitar, Take Photo, ",
+  "Games : Music games, FPS, Puzzle",
+  "Music : OST.Song, Rock, Pop, Thai Song",
+  "Analog : film Camera, "
 ]
